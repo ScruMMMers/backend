182b5ed479c005c0142a4a25396dc8b0fed190c9
Gordey Dovydenko <inostaziss@gmail.com>
2025-03-14 13:21:55 +0700

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker pull ggnathion/internship-app:latest
 docker stop internship-app || true
 docker rm internship-app || true
-docker run -d --name internship-app -p 8080:8080 ggnathion/internship-app:latest
+APP_VERSION=latest docker compose up internship-app -d
