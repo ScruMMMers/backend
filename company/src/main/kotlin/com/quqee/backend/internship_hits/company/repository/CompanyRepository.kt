126ab208deb50cc1f58386f36a3c951114669721
sitdikov <platunovab-ext@vip.cbr.ru>
2025-03-24 21:19:37 +0700

    bugfix запрос на получение списка компаний

--- company/src/main/kotlin/com/quqee/backend/internship_hits/company/repository/CompanyRepository.kt
+++ company/src/main/kotlin/com/quqee/backend/internship_hits/company/repository/CompanyRepository.kt
@@ -6,7 +6,6 @@ import com.quqee.backend.internship_hits.company.repository.jpa.CompanyJpaReposi
 import com.quqee.backend.internship_hits.public_interface.common.ShortCompanyDto
 import com.quqee.backend.internship_hits.public_interface.company.CompanyDto
 import com.quqee.backend.internship_hits.public_interface.company.CreateCompanyDto
-import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Component
 import java.time.OffsetDateTime
 import java.util.*
@@ -50,8 +49,6 @@ class CompanyRepository(
      * Получение списка компаний
      */
     fun getCompaniesList(name: String?, lastId: UUID?, pageSize: Int): List<ShortCompanyDto> {
-        val pageable = PageRequest.of(0, pageSize)
-
         var lastCompanyCreatedAt: OffsetDateTime? = null
         if (lastId != null) {
             lastCompanyCreatedAt = companyJpaRepository.findById(lastId)
@@ -59,7 +56,7 @@ class CompanyRepository(
                 .createdAt
         }
 
-        val companies = companyJpaRepository.findAllByNameLikePage(name, lastCompanyCreatedAt, pageable)
+        val companies = companyJpaRepository.findAllByNameLikePage(name, lastCompanyCreatedAt, pageSize)
 
         return companies.map { mapper.toShortCompanyDto(it) }
     }
--- company/src/main/kotlin/com/quqee/backend/internship_hits/company/repository/jpa/CompanyJpaRepository.kt
+++ company/src/main/kotlin/com/quqee/backend/internship_hits/company/repository/jpa/CompanyJpaRepository.kt
@@ -1,8 +1,6 @@
 package com.quqee.backend.internship_hits.company.repository.jpa
 
 import com.quqee.backend.internship_hits.company.entity.CompanyEntity
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -15,15 +13,17 @@ interface CompanyJpaRepository : JpaRepository<CompanyEntity, UUID> {
 
     @Query(
         value = """
-            SELECT c FROM CompanyEntity c 
-            WHERE (:name IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')))
-            AND (:createdAt IS NULL OR c.createdAt < :createdAt)
-            ORDER BY c.createdAt DESC
-        """
+        SELECT * FROM company c
+        WHERE (:name IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')))
+        AND (:createdAt IS NULL OR c.created_at < :createdAt)
+        ORDER BY c.created_at DESC
+        LIMIT :limit
+    """,
+        nativeQuery = true
     )
     fun findAllByNameLikePage(
         @Param("name") name: String?,
         @Param("createdAt") createdAt: OffsetDateTime?,
-        pageable: Pageable
-    ): Page<CompanyEntity>
+        @Param("limit") limit: Int
+    ): List<CompanyEntity>
 }
