ed9a1f94326bc0735d7e30d9296d3e066b68f0e0
sitdikov <platunovab-ext@vip.cbr.ru>
2025-03-26 00:51:29 +0700

    fix миграции последний (надеюсь)

--- migration/changelog/ddl/v5_create_comment_table.sql
+++ migration/changelog/ddl/v5_create_comment_table.sql
@@ -3,8 +3,8 @@ CREATE TABLE comment
     id         UUID PRIMARY KEY,
     author_id  UUID        NOT NULL,
     message    TEXT        NOT NULL,
-    replyTo    UUID        NOT NULL,
+    reply_to    UUID        NOT NULL,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NULL,
-    isDeleted  BOOLEAN     NOT NULL
+    is_deleted  BOOLEAN     NOT NULL
 )
