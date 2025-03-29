a01052fc32336ff2043dafa7e8ac8f62bf46e609
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-03-30 01:29:16 +0700

    Модуль тегов разделен на запись и чтение

--- api/rest/src/main/kotlin/com/quqee/backend/internship_hits/api/rest/TagsController.kt
+++ api/rest/src/main/kotlin/com/quqee/backend/internship_hits/api/rest/TagsController.kt
@@ -3,17 +3,17 @@ package com.quqee.backend.internship_hits.api.rest
 import com.quqee.backend.internship_hits.mapper.FromInternalToApiMapper
 import com.quqee.backend.internship_hits.model.rest.TagsListView
 import com.quqee.backend.internship_hits.public_interface.tags.TagListDto
-import com.quqee.backend.internship_hits.tags.service.TagService
+import com.quqee.backend.internship_hits.tags_query.service.TagQueryService
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 
 @Component
 class TagsController(
-    private val tagsService: TagService,
+    private val tagsQueryService: TagQueryService,
     private val mapTagsList: FromInternalToApiMapper<TagsListView, TagListDto>
 ) : TagsApiDelegate {
     override fun tagsGet(name: String?): ResponseEntity<TagsListView> {
-        val tagsListDto = tagsService.getTagsByNamePart(name)
+        val tagsListDto = tagsQueryService.getTagsByNamePart(name)
         return ResponseEntity.ok(mapTagsList.fromInternal(tagsListDto))
     }
 }
---
+++ tags_query/src/main/kotlin/com/quqee/backend/internship_hits/tags_query/service/TagQueryService.kt
@@ -0,0 +1,44 @@
+package com.quqee.backend.internship_hits.tags_query.service
+
+import com.quqee.backend.internship_hits.public_interface.tags.TagListDto
+import com.quqee.backend.internship_hits.tags.entity.TagEntity
+import com.quqee.backend.internship_hits.tags.repository.TagJpaRepository
+import com.quqee.backend.internship_hits.tags_query.mapper.TagQueryMapper
+import org.springframework.stereotype.Service
+
+interface TagQueryService {
+    fun getTagsByNamePart(name: String?): TagListDto
+    fun getTagsEntityByNamePart(name: String?): List<TagEntity>
+}
+
+@Service
+class TagQueryServiceImpl(
+    private val tagJpaRepository: TagJpaRepository,
+    private val tagQueryMapper: TagQueryMapper
+) : TagQueryService {
+    /**
+     * Получение списка тегов, где имя содержит часть строки
+     * Если строка пустая, возвращаем все теги
+     */
+    override fun getTagsByNamePart(name: String?): TagListDto {
+        val tags = if (name.isNullOrBlank()) {
+            tagJpaRepository.findAll()
+        } else {
+            tagJpaRepository.findByNameContainingIgnoreCase(name)
+        }
+        return tagQueryMapper.toTagListDto(tags)
+    }
+
+    /**
+     * Получение списка сущностей тегов, где имя содержит часть строки
+     * Если строка пустая, возвращаем все теги
+     */
+    override fun getTagsEntityByNamePart(name: String?): List<TagEntity> {
+        val tags = if (name.isNullOrBlank()) {
+            tagJpaRepository.findAll()
+        } else {
+            tagJpaRepository.findByNameContainingIgnoreCase(name)
+        }
+        return tags
+    }
+}
