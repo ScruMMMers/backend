0d44f774645fa1945c5e25e0986b2e8ff6cb6dc6
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-04-08 23:11:50 +0700

    Удалил лишний импорт репозитория

--- logs/src/main/kotlin/com/quqee/backend/internship_hits/logs/LogsConfig.kt
+++ logs/src/main/kotlin/com/quqee/backend/internship_hits/logs/LogsConfig.kt
@@ -8,11 +8,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @EnableJpaRepositories(basePackages = [
     "com.quqee.backend.internship_hits.logs.repository.jpa",
     "com.quqee.backend.internship_hits.tags.repository",
-    "com.quqee.backend.internship_hits.position.repository",
 ])
 @EntityScan(basePackages = [
     "com.quqee.backend.internship_hits.logs.entity",
     "com.quqee.backend.internship_hits.tags.entity",
-    "com.quqee.backend.internship_hits.position.entity",
 ])
 open class LogsConfig
