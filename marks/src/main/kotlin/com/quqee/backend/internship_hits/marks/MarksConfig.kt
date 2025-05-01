df41c1934b06810488f8bb3e848eb93691fe206b
sitdikov <platunovab-ext@vip.cbr.ru>
2025-05-01 17:17:52 +0700

    fix

--- marks/src/main/kotlin/com/quqee/backend/internship_hits/marks/MarksConfig.kt
+++ marks/src/main/kotlin/com/quqee/backend/internship_hits/marks/MarksConfig.kt
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(scanBasePackages = ["com.quqee.backend.internship_hits.marks", "com.quqee.backend.internship_hits.students"])
 @EnableJpaRepositories(basePackages = ["com.quqee.backend.internship_hits.marks.repository"])
-@EntityScan(basePackages = ["com.quqee.backend.internship_hits.meeting.entity"])
+@EntityScan(basePackages = ["com.quqee.backend.internship_hits.marks.entity"])
 open class MarksConfig
--- marks/src/main/kotlin/com/quqee/backend/internship_hits/marks/repository/MarkRepository.kt
+++ marks/src/main/kotlin/com/quqee/backend/internship_hits/marks/repository/MarkRepository.kt
@@ -2,11 +2,11 @@ package com.quqee.backend.internship_hits.marks.repository
 
 import com.quqee.backend.internship_hits.marks.entity.MarkEntity
 import com.quqee.backend.internship_hits.public_interface.common.UserId
-import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.Optional
-import java.util.UUID
+import org.springframework.stereotype.Repository
+import java.util.*
 
+@Repository
 interface MarkRepository : JpaRepository<MarkEntity, UUID> {
 
     fun findByUserIdAndSemester(userId: UserId, semester: Int): Optional<MarkEntity>
