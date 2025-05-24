2dcd28774fb2a0d5d861966207d7eb5c2568cf84
staziss <tghkfjrjrjlyfbjrjjfhu@gmail.com>
2025-05-24 22:01:52 +0700

    remove logic to send notification

--- announcement/src/main/kotlin/com/quqee/backend/internship_hits/announcement/AnnouncementBatchDispatcher.kt
+++ announcement/src/main/kotlin/com/quqee/backend/internship_hits/announcement/AnnouncementBatchDispatcher.kt
@@ -2,12 +2,10 @@ package com.quqee.backend.internship_hits.announcement
 
 import com.quqee.backend.internship_hits.notification.service.NotificationService
 import com.quqee.backend.internship_hits.public_interface.notification_public.CreateNotificationDto
-import com.quqee.backend.internship_hits.public_interface.notification_public.SendNotificationDto
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
 open class AnnouncementBatchDispatcher(
@@ -23,25 +21,10 @@ open class AnnouncementBatchDispatcher(
         try {
             log.info("Отправка пачки ${notificationDtos.size} уведомлений")
             notificationService.createNotifications(notificationDtos)
-
-            //TODO: Временное решение для тестов. УДАЛИТЬ, как будет настроен CDC
-            val notifications = notificationDtos.map { mapCreateNotificationToSend(it) }
-            notificationService.sendNotifications(notifications)
         } catch (ex: Exception) {
             log.error("Ошибка отправки пачки уведомлений", ex)
         } finally {
             onComplete()
         }
-    }
-
-    private fun mapCreateNotificationToSend(dto: CreateNotificationDto): SendNotificationDto {
-        return SendNotificationDto(
-            title = dto.title,
-            message = dto.message,
-            userId = dto.userId,
-            channels = dto.channels,
-            type = dto.type,
-            id = UUID.randomUUID()
-        )
     }
 }
