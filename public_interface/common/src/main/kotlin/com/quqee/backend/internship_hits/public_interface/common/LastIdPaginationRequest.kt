cd41f5c0c2b715a1c3455dfe19a92137ec81f581
staziss <tghkfjrjrjlyfbjrjjfhu@gmail.com>
2025-05-25 21:57:15 +0700

    fix pagination in notification

--- public_interface/common/src/main/kotlin/com/quqee/backend/internship_hits/public_interface/common/LastIdPaginationRequest.kt
+++ public_interface/common/src/main/kotlin/com/quqee/backend/internship_hits/public_interface/common/LastIdPaginationRequest.kt
@@ -1,9 +1,12 @@
 package com.quqee.backend.internship_hits.public_interface.common
 
+import java.time.OffsetDateTime
+
 data class LastIdPaginationRequest<IdType>(
     val lastId: IdType? = null,
     private val pageSize: Int? = DEFAULT_PAGE_SIZE,
     val sorting: SortingStrategy = SortingStrategy.CREATED_AT_ASC,
+    val lastDate: OffsetDateTime? = null,
 ) {
     val size: Int = pageSize ?: DEFAULT_PAGE_SIZE
     val sizeForSelect get() = size + 1
