5d116d750bf1063bb6260e26e29a486f9dacbbae
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-06-17 00:10:39 +0700

    Исправлена ошибка репозитория

--- semester/src/main/kotlin/com/quqee/backend/internship_hits/semester/repository/AcademicYearRepository.kt
+++ semester/src/main/kotlin/com/quqee/backend/internship_hits/semester/repository/AcademicYearRepository.kt
@@ -2,8 +2,10 @@ package com.quqee.backend.internship_hits.semester.repository
 
 import com.quqee.backend.internship_hits.semester.entity.AcademicYearEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.UUID
 
+@Repository
 interface AcademicYearRepository : JpaRepository<AcademicYearEntity, UUID> {
     fun findByStartYear(startYear: Int): AcademicYearEntity?
 }
