67f64ee1d91450ea009d07094941babd0b628651
Gordey Dovydenko <inostaziss@gmail.com>
2025-06-29 22:05:03 +0700

    added filter for announcement

--- api/rest/src/main/kotlin/com/quqee/backend/internship_hits/api/mapper/announcement/StudentFilterMapperConfiguration.kt
+++ api/rest/src/main/kotlin/com/quqee/backend/internship_hits/api/mapper/announcement/StudentFilterMapperConfiguration.kt
@@ -1,5 +1,7 @@
 package com.quqee.backend.internship_hits.api.mapper.announcement
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.quqee.backend.internship_hits.mapper.EnumerationMapper
 import com.quqee.backend.internship_hits.mapper.FromApiToInternalMapper
 import com.quqee.backend.internship_hits.mapper.makeFromApiMapper
@@ -11,6 +13,7 @@ import com.quqee.backend.internship_hits.public_interface.common.enums.Position
 import com.quqee.backend.internship_hits.public_interface.enums.ApprovalStatus
 import com.quqee.backend.internship_hits.public_interface.enums.LogType
 import com.quqee.backend.internship_hits.public_interface.students_public.GetStudentsListFilterParamDto
+import com.quqee.backend.internship_hits.public_interface.students_public.LogByCompanyItem
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -18,7 +21,8 @@ import org.springframework.context.annotation.Configuration
 class StudentFilterMapperConfiguration(
     private val mapLogType: EnumerationMapper<LogTypeEnum, LogType>,
     private val mapApprovalStatus: EnumerationMapper<ApprovalStatusEnum, ApprovalStatus>,
-    private val mapPositionType: EnumerationMapper<PositionEnum, Position>
+    private val mapPositionType: EnumerationMapper<PositionEnum, Position>,
+    private val objectMapper: ObjectMapper,
 ) {
 
     @Bean
@@ -32,7 +36,12 @@ class StudentFilterMapperConfiguration(
                 positionType = view.positionType?.map { mapPositionType.mapToInternal(it) }?.toSet(),
                 positionName = view.positionName?.toSet(),
                 companyIds = view.companyIds?.toSet(),
-                name = view.name
+                name = view.name,
+                withoutCompanies = view.withoutCompanies == true,
+                withoutLogs = view.withoutLogs == true,
+                logByCompany = view.logByCompany?.let {
+                    objectMapper.readValue(it, object : TypeReference<Map<LogType, LogByCompanyItem>>() {})
+                },
             )
         }
 }
